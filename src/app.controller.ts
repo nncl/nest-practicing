58b6d60d8a384f39c22833e74deff1ee593e77cd
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-10-11 17:47:00 -0300

    feat(auth): Includes Guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const token = request.headers['authorization'];
+
+    if (!token) {
+      throw new UnauthorizedException('Token is missing');
+    }
+
+    return true;
+  }
+}
