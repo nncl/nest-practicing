f7c1c61a57cfc4d46d25a006090fdcf5204fcf64
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-10-16 10:50:50 -0300

    test: Validates methods of AppController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth/auth.guard';
 
-describe('AppController', () => {
+describe(AppController.name, () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(['user1', 'user2']),
+            findOne: jest.fn().mockResolvedValue('user1'),
+          },
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: jest.fn().mockReturnValue(true) }) // Mocking the AuthGuard
+      .compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
+  });
+
+  describe('getUsers', () => {
+    it('should return an array of users', async () => {
+      const result = await appController.getUsers();
+      expect(result).toEqual(['user1', 'user2']);
+      expect(appService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('getUserById', () => {
+    it('should return a single user', async () => {
+      const result = await appController.getUserById({ id: '1' });
+      expect(result).toEqual('user1');
+      expect(appService.findOne).toHaveBeenCalledWith('1');
     });
   });
 });
