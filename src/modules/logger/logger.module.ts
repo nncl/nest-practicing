a797edd6817cea7c58d9eeed00f43f1c0742ca4c
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-10-18 18:05:06 -0300

    feat(modules): Provides PubSub service

--- src/modules/logger/logger.module.ts
+++ src/modules/logger/logger.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MyLoggerService } from './services/my-logger/my-logger.service';
+import { PubSubService } from './services/pub-sub/pub-sub.service';
 
 @Module({
-  providers: [MyLoggerService],
+  providers: [
+    MyLoggerService,
+    {
+      provide: 'IPubSubService',
+      useClass: PubSubService,
+    },
+  ],
 })
 export class LoggerModule {}
