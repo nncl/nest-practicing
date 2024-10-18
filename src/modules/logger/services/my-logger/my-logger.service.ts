c4650b238186e3fcf03cba1a90812700cd42f128
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-10-18 18:07:40 -0300

    feat(services): Sends logs to PubSub Service

--- src/modules/logger/services/my-logger/my-logger.service.ts
+++ src/modules/logger/services/my-logger/my-logger.service.ts
@@ -1,11 +1,50 @@
-import { ConsoleLogger, Injectable } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 
 import * as winston from 'winston';
+import { IPubSubService } from '../../dto/pub-sub';
 
 @Injectable()
-export class MyLoggerService extends ConsoleLogger {
-  log(message: any, context?: string) {
-    winston.log('info', message);
-    super.log(message, context);
+export class MyLoggerService implements LoggerService {
+  private logger: winston.Logger;
+
+  private readonly pubSubTopic = 'logs';
+
+  constructor(
+    @Inject('IPubSubService') private readonly pubSubService: IPubSubService,
+  ) {
+    this.logger = winston.createLogger({
+      level: 'info',
+      transports: [
+        new winston.transports.Console({ format: winston.format.simple() }),
+      ],
+    });
+  }
+
+  async log(message: string, context?: string) {
+    this.logger.info(message);
+    await this.pubSubService.publishLog(this.pubSubTopic, {
+      level: 'info',
+      message,
+      context,
+    });
+  }
+
+  async error(message: string, trace: string, context?: string) {
+    this.logger.error(message);
+    await this.pubSubService.publishLog(this.pubSubTopic, {
+      level: 'error',
+      message,
+      trace,
+      context,
+    });
+  }
+
+  async warn(message: string, context?: string) {
+    this.logger.warn(message);
+    await this.pubSubService.publishLog(this.pubSubTopic, {
+      level: 'warn',
+      message,
+      context,
+    });
   }
 }
