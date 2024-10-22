cd590a50e6110f03f1a365a6f7c87cd7fd641dda
Marcos Tesolin Munhoz <marcos.munhoz@telefonicabigdata.onmicrosoft.com>
2024-10-22 11:05:13 -0300

    refactor(logger): replace coupled pubsub logger with an abstract log transport implementation in logger service

--- src/modules/logger/services/my-logger/my-logger.service.ts
+++ src/modules/logger/services/my-logger/my-logger.service.ts
@@ -1,16 +1,15 @@
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
 
 import * as winston from 'winston';
-import { IPubSubService } from '../../dto/pub-sub';
+import { LogTransportToken } from '../../tokens';
+import { LogLevel, LogTransport } from '../../transports/log.transport.interface';
 
 @Injectable()
 export class MyLoggerService implements LoggerService {
   private logger: winston.Logger;
 
-  private readonly pubSubTopic = 'logs';
-
   constructor(
-    @Inject('IPubSubService') private readonly pubSubService: IPubSubService,
+    @Inject(LogTransportToken) private readonly logTransport: LogTransport,
   ) {
     this.logger = winston.createLogger({
       level: 'info',
@@ -22,8 +21,8 @@ export class MyLoggerService implements LoggerService {
 
   async log(message: string, context?: string) {
     this.logger.info(message);
-    await this.pubSubService.publishLog(this.pubSubTopic, {
-      level: 'info',
+    await this.logTransport.send({
+      level: LogLevel.INFO,
       message,
       context,
     });
@@ -31,8 +30,8 @@ export class MyLoggerService implements LoggerService {
 
   async error(message: string, trace: string, context?: string) {
     this.logger.error(message);
-    await this.pubSubService.publishLog(this.pubSubTopic, {
-      level: 'error',
+    await this.logTransport.send({
+      level: LogLevel.ERROR,
       message,
       trace,
       context,
@@ -41,8 +40,8 @@ export class MyLoggerService implements LoggerService {
 
   async warn(message: string, context?: string) {
     this.logger.warn(message);
-    await this.pubSubService.publishLog(this.pubSubTopic, {
-      level: 'warn',
+    await this.logTransport.send({
+      level: LogLevel.WARN,
       message,
       context,
     });
