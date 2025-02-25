a7ca363446866bb7929c303e162bbb0ccf494dcb
Caue Bruno De Almeida <caue.almeida@telefonica.com>
2025-02-25 08:50:47 -0300

    feat: Validates query params

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (errors) => new BadRequestException(errors),
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
--- src/modules/form/form.controller.ts
+++ src/modules/form/form.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Query, UseFilters } from '@nestjs/common';
+import { Type } from 'class-transformer';
 import {
-    IsEmail,
-    IsNotEmpty,
-    IsNumber,
-    IsOptional,
-    IsString,
+  IsDefined,
+  IsEmail,
+  IsIn,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
 } from 'class-validator';
+import { ValidationFilter } from '../../filters/errors/validation/validation.filter';
 
 class CreateUserDto {
   @IsNotEmpty()
@@ -20,10 +26,26 @@ class CreateUserDto {
   age?: number;
 }
 
+class CreateUserQueryDto {
+  @IsOptional()
+  @IsIn(['F', 'M'])
+  gender: 'F' | 'M';
+
+  @IsDefined()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  page: number;
+}
+
+@UseFilters(ValidationFilter)
 @Controller('form')
 export class FormController {
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return createUserDto;
+  create(
+    @Body() createUserDto: CreateUserDto,
+    @Query() query: CreateUserQueryDto,
+  ) {
+    return { createUserDto, query };
   }
 }
